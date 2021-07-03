84cc44b43b17ba0890ab6b9f7ac5ceec9cb6ce23
alauddin <alauddin1232002@gmail.com>
2021-07-03 15:16:14 +0530

    second attempt for azure

--- src/main/java/com/javatech/SpringbootazuredemoApplication.java
+++ src/main/java/com/javatech/SpringbootazuredemoApplication.java
@@ -11,7 +11,7 @@ public class SpringbootazuredemoApplication {
 	
 	@GetMapping("/pgr")
 	public String getmessage() {
-		return "my first program on azure with second deployment done";
+		return "my first program on azure with second deployment done second attempt";
 	}
 
 	public static void main(String[] args) {
